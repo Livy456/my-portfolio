69c8f8b6169c3f108ed03babfbe9fbc99abf4b4d
Olivia <52472697+Livy456@users.noreply.github.com>
2024-03-13 20:59:12 -0400

    relocated color scheme switch from global.js into +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,30 @@
     // import '../../static/style.css';
     import Navbar from "../lib/Navbar.svelte";
 
+    let colorScheme = "light dark";
+    let root = globalThis?.document?.documentElement;
+    $: root?.style.setProperty("color-scheme", colorScheme);
+
 </script>
+<style> 
+label.color-scheme{
+    position: absolute;
+    top: 0.5em;
+    right: 0.5rem;
+    padding-left: 0.5rem;
+    font-family: inherit;
+}
+</style>
 <Navbar></Navbar>
+
+<!-- <p>{colorScheme}</p> -->
+<label class="color-scheme">
+    Theme:
+    <select bind:value={ colorScheme }>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+    </select>
+</label>
 
 <slot />
--- static/global.js
+++ static/global.js
@@ -1,69 +1,27 @@
-// Automatic Navigation Menu
-// let pages = [
-//     {url: "./", title: "Home"},
-//     {url: "projects", title: "Projects"},
-//     {url: "contact", title: "Contact"},
-//     {url: "https://livy456.github.io/portfolio/", title: "Portfolio"},
-//     {url: "resume", title: "Resume"},
-// ];
-// const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// add option to change the color scheme of the page
+// document.body.insertAdjacentHTML("afterbegin", 
+// `<label class="color-scheme">
+//     Theme:
+//     <select>
+//         <option value="light dark">Automatic</option>
+//         <option value="light">Light</option>
+//         <option value="dark">Dark</option>
+//     </select>
+// </label>
+// `);
 
-// let nav = document.createElement("nav");
-// document.body.prepend(nav);
+// let select = document.querySelector("label");
 
-// //add a link to each page on the navigation menu
-// for (let p of pages)
-// {
-//     let url = p.url;
-//     let title = p.title;
-
-//     // create link and add it to nav menu
-//     let a = document.createElement("a");
-    
-//     // not needed when using svelte
-//     // if (!ARE_WE_HOME && !url.startsWith("http"))
-//     // {
-//     //     url = "../" + url
-//     // }
-
-//     a.href = url;
-//     a.textContent = title;
-
-//     if (a.host === location.host && a.pathname === location.pathname)
-//     {
-//         a.classList.add("current");
-//     }
-
-//     else if (a.host !== location.host)
-//     {
-//         a.target = "_blank";
-//     }
-
-//     nav.append(a);
-
-    // add option to change the color scheme of the page
-    document.body.insertAdjacentHTML("afterbegin", 
-    `<label class="color-scheme">
-        Theme:
-        <select>
-            <option value="light dark">Automatic</option>
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-        </select>
-    </label>
-    `);
-
-    let select = document.querySelector("select");
-    if ("colorScheme" in localStorage)
-    {   
-        document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme);
-        select.value = localStorage.colorScheme;
-    }
-    
-    select.addEventListener("input", function (event){
-        
-        document.documentElement.style.setProperty("color-scheme", event.target.value);
-        localStorage.colorScheme = event.target.value;
-    });
+// if ("colorScheme" in localStorage)
+// {   
+//     document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme);
+//     select.value = localStorage.colorScheme;
 // }
+
+// select.addEventListener("input", function (event){
+    
+//     document.documentElement.style.setProperty("color-scheme", event.target.value);
+//     localStorage.colorScheme = event.target.value;
+// });
+
 
