7ef3a90e4a34eaf31948d1f6eac3d1e4a7d43749
Olivia <52472697+Livy456@users.noreply.github.com>
2024-03-31 09:38:20 -0400

    update +page.svelte

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -3,10 +3,47 @@
     import { onMount } from "svelte";
 
     let data = [];
+    let commits = [];
 
     onMount(async() => {
-        data = await d3.csv("loc.csv");
+        data = await d3.csv("loc.csv", row=> ({
+            ...row,
+            line: Number(row.line),
+            depth: Number(row.depth),
+            length: Number(row.length),
+            date: new Date(row.date + "T00:00" + row.timezone),
+            datetime: new Date(row.datetime)
+        }));
+
+        commits = d3.groups(data, d => d.commit).map( ([commit, lines]) => {
+            let first = lines[0];
+            let {author, date, time, timezone, datetime} = first;
+            let ret = {
+                id: commit,
+                url: "https://github.com/Livy456/my-portfolio" + commit,
+                author, date, time, timezone, datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length
+            };
+
+            Object.defineProperty(ret, "lines", {
+                values: lines,
+                configurable: true,
+                writable: true,
+                enumerable: false
+            });
+
+            return ret;
+
+        });
+
+        console.log(commits);
     });
+
+
+
+    
 </script>
 
 <p>Total lines of code: {data.length}</p>
+<!-- <p>{JSON.stringify(commits, null, "\t")}</p> -->
